eb542f59c3dc24fb2b866957f02d3c6a87b9ce6b
Dmitriy P <my@bootmail.ru>
2023-06-16 14:26:21 +0300

    added solution for task44

--- task_44.py
+++ task_44.py
@@ -9,3 +9,15 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI'lst})
 data.head()
 """
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+print(*lst)
+data = pd.DataFrame({'whoAmI': lst})
+# table = pd.get_dummies(data['whoAmI']).astype(int)
+table = pd.DataFrame({'human': data['whoAmI'] == 'human',
+                      'robot': data['whoAmI'] == 'robot'}).astype(int)  # .head(len(lst))
+print(table)
